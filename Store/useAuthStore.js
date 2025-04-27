2a6c59888905ad80e9da3de9cdb630dbe522ee31
mahtabmunsur <132754938+Mahtabracer@users.noreply.github.com>
2025-04-27 23:11:33 +0600

    Add files via upload

---
+++ Store/useAuthStore.js
@@ -0,0 +1,19 @@
+import { create } from 'zustand';
+
+export const useAuthStore = create((set) => ({
+  user: null,
+  token: localStorage.getItem('token') || null,
+
+  login: (user, token) => {
+    localStorage.setItem('token', token);
+    set({ user, token });
+  },
+
+  logout: () => {
+    localStorage.removeItem('token');
+    set({ user: null, token: null });
+  },
+}));
+const { logout } = useAuthStore();
+
+<button onClick={logout}>Logout</button>
